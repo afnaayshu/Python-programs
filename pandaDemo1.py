78914968185fe851ff4d622d306d7ecda9919ba0
Afna Ayshu Jaffin <76438840+afnaayshu@users.noreply.github.com>
2023-06-09 10:30:15 +0530

    Create pandaDemo1.py

---
+++ pandaDemo1.py
@@ -0,0 +1,13 @@
+import pandas as pd
+X = [3,4,5,6,7,8]
+var1 = pd.Series(X,index = ['a','b','c','d','e','f'],dtype = float,name = "Python")
+names = ['Afna','Meril','Gopika','Melissa']
+var = pd.Series(names,index =['a','b','c','d'])
+dic = {"name":["Python","C","C++","Java"],"Rank":[1,4,3,2]}
+var2 = pd.Series(dic)
+s = pd.Series(12,index=[1,2,3,4,5,6])
+print(s)
+print(type(s))
+print(var1)
+print(var2)
+print(var)
